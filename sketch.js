b99052ba595654e4dec74a686827af514500d59a
kristian003 <kristian003@gmail.com>
2016-10-24 11:59:34 -0400

    Commit4

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ var spot = {
   y: 200
 };
 
-var colEmotions = {
-  angry: red,
-  sad: blue,
+var colAngry = {
+  r: 0,
+  g: 255,
   b:100
 };
 
@@ -14,15 +14,15 @@ var colEmotions = {
 
 function setup() {
   createCanvas(600, 400);
-  Slider1 = createSlider("angry");
+  Slider1 = createSlider("colAngry");
   Slider1.position(20, 20);
  
 }
 
 function draw() {
-  // var angry = Slider1.red();
-  // fill(angry);
-  // // noStroke();
+  var ColAngry = Slider1.value();
+  fill("colAngry");
+  // noStroke();
   ellipse(spot.x, spot.y, 30, 30);
   
   
