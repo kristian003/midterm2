6f682b5d99282a2a1e2b8786ef57a0a92fcaf986
kristian003 <kristian003@gmail.com>
2016-10-24 15:13:03 -0400

    commit5

--- sketch.js
+++ sketch.js
@@ -1,23 +1,37 @@
-var rSlider
-
 var spot = {
   x: 300,
   y: 200
 };
 
-var colAngry = "red";
 
+var colAngry = {
+  r: 0,
+  g:255,
+  b:100
+};
+
+
+// var rSlider;
 
 function setup() {
   createCanvas(600, 400);
-  Slider1 = createSlider(colAngry);
-  Slider1.position(20, 20);
- 
+  rSlider = createSlider(0, 255, 100);
+  rSlider.position(20, 20);
+  gSlider = createSlider(0, 255, 0);
+  gSlider.position(20, 50);
+  bSlider = createSlider(0, 255, 255);
+  bSlider.position(20, 80);
+  bSlider2 = createSlider(0, 255, 255);
+  bSlider2.position(20, 110);
 }
 
 function draw() {
-  var colAngry = Slider1.value();
-  fill('blue');
+  var r = rSlider.value();
+  var g = gSlider.value();
+  var b = bSlider.value();
+  var b = bSlider2.value();
+  fill(r, g, b);
+  noStroke();
   ellipse(spot.x, spot.y, 30, 30);
   
   
