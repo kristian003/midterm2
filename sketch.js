581f12545882b86f192bfd45fcf155d6ec3b4ef8
kristian003 <kristian003@gmail.com>
2016-10-26 09:29:48 -0400

    Midterm4

--- sketch.js
+++ sketch.js
@@ -1,38 +1,37 @@
+var alf = 0;
+var beta = 0;
+var gamma = 0
+
+var alfbutton;
+var betabutton;
+var gammabutton;
+
+
 var spot = {
   x: 300,
   y: 200
 };
 
 
-var colAngry = {
-  r: 0,
-  g:255,
-  b:100
-};
-
-
-var rSlider;
 
 function setup() {
   createCanvas(600, 400);
-  rSlider = createSlider(0, 255, 255);
+  rSlider = createSlider(0, 255, 100);
   rSlider.position(20, 20);
   gSlider = createSlider(0, 255, 0);
   gSlider.position(20, 50);
-  bSlider = createSlider(0, 0, 255);
+  bSlider = createSlider(0, 255, 255);
   bSlider.position(20, 80);
-  bSlider2 = createSlider(0, 0, 255);
-  bSlider2.position(20, 110);
+ 
 }
 
 function draw() {
   var r = rSlider.value();
   var g = gSlider.value();
   var b = bSlider.value();
-  var b = bSlider2.value();
-  fill(r, g, b);
+  fill(r,g,b);
   noStroke();
-  ellipse(spot.x, spot.y, 30, 30);
+  ellipse(spot.x, spot.y, 100, 100);
   
   
   
