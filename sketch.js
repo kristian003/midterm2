8eb0718ae76f91aed2f791d7d4301a4e36ab9a2a
kristian003 <kristian003@gmail.com>
2016-10-25 21:25:50 -0400

    Midterm3

--- sketch.js
+++ sketch.js
@@ -1,41 +1,79 @@
-var spot = {
-  x: 300,
-  y: 200
-};
+var alf = 0;
+var beta = 0;
+var gamma = 0
 
+var alfbutton;
+var betabutton;
+var gammabutton;
 
-var colAngry = {
-  r: 0,
-  g:255,
-  b:100
-};
+var bubble1;
 
+var alfabool = false;
 
-var rSlider;
+var alfaArray = [];
+
 
 function setup() {
   createCanvas(600, 400);
-  rSlider = createSlider(0, 255, 255);
-  rSlider.position(20, 20);
-  gSlider = createSlider(0, 255, 0);
-  gSlider.position(20, 50);
-  bSlider = createSlider(0, 0, 255);
-  bSlider.position(20, 80);
-  bSlider2 = createSlider(0, 0, 255);
-  bSlider2.position(20, 110);
+  alfbutton = createButton("alpha");
+  alfbutton.position(10, 10);
+  alfbutton.mousePressed(alfarandom);
+
+  bubble1 = new alfabubble(random(width), random(height));
+
 }
 
 function draw() {
-  var r = rSlider.value();
-  var g = gSlider.value();
-  var b = bSlider.value();
-  var b = bSlider2.value();
-  fill(r, g, b);
-  noStroke();
-  ellipse(spot.x, spot.y, 30, 30);
+  frameRate(1);
   
+  background(255);
   
+  if (alfabool == true) {
+    var randomalf = random(7.5, 12.6);
+    alf = int(map(randomalf, 7.5, 12.6, 0, 50));
+    console.log(alf);
+    
+    for(var i = 0; i < alf; i++){
+      alfaArray.push(new alfabubble(random(width), random(height)));
+    }
+    
+    for(var i = 0; i < alfaArray.length; i++){
+      alfaArray[i].display();
+    }
+    
+    alfaArray = [];
+  }
   
+  // create betaboolsection
+  // create gammaboolsection
+
+ // bubble1.display();
+  
+
+
+}
+
+function alfarandom() {
+  if (alfabool == false) {
+    alfabool = true;
+  
+  } else {
+    alfabool = false;
+  }
+  console.log(alfabool);
 }
 
 
+
+
+function alfabubble(tempX, tempY) {
+  this.x = tempX;
+  this.y = tempY;
+  this.diameter = 50;
+
+  this.display = function() {
+    fill("green");
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+
+}
